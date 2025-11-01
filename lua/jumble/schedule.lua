2e4a647f1a5d371b66f849ba185d874b951cf043
mjcevallos007@gmail.com <devy@localhost.localdomain>
2025-11-01 17:32:49 -0400

    adding stuff from here

--- lua/jumble/schedule.lua
+++ lua/jumble/schedule.lua
@@ -2,7 +2,9 @@ local M = {}
 
 ---The main instance is responsible for changing the colorscheme
 ---TODO: Continue from here (for real)
-function M.schedule_colorscheme_change() end
+function M.schedule_colorscheme_change()
+	-- Create the scheduler for the colorscheme change
+end
 
 ---@return table M Scheduling methods for this
 return M
