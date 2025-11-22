899cff277918767aedc6e73b9fe67be1b0d673a5
anonymous <mjcevallos007@gmail.com>
2025-11-21 23:14:53 -0500

    added the autocmd for closing

--- lua/jumble/utils.lua
+++ lua/jumble/utils.lua
@@ -30,7 +30,13 @@ function M.init(opts)
 			schedule.schedule_colorscheme_change(themes, timeoptions)
 			-- Additionally, if this instance is closed, make sure that we then again, remove the lock file and also re run the acquire lock part
 			-- TODO: Add the autocmd to release (delete file) lock when the instance closes
+			vim.api.nvim_create_autocmd({ "QuitPre" }, {
+				callback = function()
+					lock.release_lock()
+				end,
+			})
 		end
+
 		-- Watch for changes
 		watch.watch_colorscheme()
 	end)
